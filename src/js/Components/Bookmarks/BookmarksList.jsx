e145569a2218d356cc753981bd51303e71c78684
Ilya Altukhov <zompin@ya.ru>
2019-03-18 23:06:57 +0300

    Enable edit if no one bookmark

--- src/js/Components/Bookmarks/BookmarksList.jsx
+++ src/js/Components/Bookmarks/BookmarksList.jsx
@@ -1,41 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import BookmarksItem from './BookmarkItem';
 import BookmarkAdd from './BookmarkAdd';
+import { toggleBookmarks } from '../../Actions/Bookmarks';
 
-const BookmarksList = ({
-  bookmarks,
-  isEditable,
-}) => (
-  <div className="bookmarks">
-    {
-      !!bookmarks.length && bookmarks.map(b => (
-        <BookmarksItem
-          key={b.id}
-          id={b.id}
-          url={b.url}
-          title={b.title}
-          isEditable={isEditable}
-        />
-      ))
+class BookmarksList extends Component {
+  componentDidUpdate(prevProps) {
+    const {
+      isEditable,
+      isLoaded,
+      bookmarks,
+      toggleBookmarks,
+    } = this.props;
+
+    if (isLoaded && !prevProps.isLoaded && !isEditable && !bookmarks.length) {
+      toggleBookmarks();
     }
-    {
-      (!bookmarks.length || isEditable) && <BookmarkAdd />
-    }
-  </div>
-);
+  }
+
+  render() {
+    const { bookmarks, isEditable } = this.props;
+
+    return (
+      <div className="bookmarks">
+        {
+          bookmarks.map(b => (
+            <BookmarksItem
+              key={b.id}
+              id={b.id}
+              url={b.url}
+              title={b.title}
+              isEditable={isEditable}
+            />
+          ))
+        }
+        {
+          isEditable && <BookmarkAdd />
+        }
+      </div>
+    );
+  }
+}
 
 BookmarksList.propTypes = {
   bookmarks: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   isEditable: PropTypes.bool.isRequired,
+  isLoaded: PropTypes.bool.isRequired,
+  toggleBookmarks: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     bookmarks: state.Bookmarks.bookmarks,
     isEditable: state.Bookmarks.isBookmarksEditable,
+    isLoaded: state.Bookmarks.isBookmarksLoaded,
   };
 }
 
-export default connect(mapStateToProps)(BookmarksList);
+export default connect(mapStateToProps, { toggleBookmarks })(BookmarksList);
