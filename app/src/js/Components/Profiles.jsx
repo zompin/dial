900b51f9c116f67890ad7e48545e7aeb3a5fd30d
Ilya Altukhov <zompin@ya.ru>
2020-01-30 00:04:03 +0300

    change profile hotkeys

--- app/src/js/Components/Profiles.jsx
+++ app/src/js/Components/Profiles.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import cs from 'classnames';
 import EditButton from './EditButton';
@@ -7,14 +7,42 @@ import { setProfile } from '../Actions/Profiles';
 
 const Profiles = () => {
   const [isPopup, setPopup] = useState(false);
+  const dataRef = useRef([]);
+  const currentRef = useRef('');
   const dispatch = useDispatch();
   const data = useSelector((state) => state.Profiles.data);
   const isLoaded = useSelector((state) => state.Profiles.isLoaded);
   const currentProfile = useSelector((state) => state.Profiles.current);
+  dataRef.current = data;
+  currentRef.current = currentProfile;
 
   const onSelect = (id) => {
     dispatch(setProfile(id));
   };
+
+  const onCommand = (command) => {
+    const index = dataRef.current.findIndex((p) => p.id === currentRef.current);
+
+    if (index === -1) {
+      return;
+    }
+
+    if (command === 'profile-next' && index < dataRef.current.length - 1) {
+      onSelect(dataRef.current[index + 1].id);
+    }
+
+    if (command === 'profile-prev' && index > 0) {
+      onSelect(dataRef.current[index - 1].id);
+    }
+  };
+
+  useEffect(() => {
+    browser.commands.onCommand.addListener(onCommand);
+
+    return () => {
+      browser.commands.onCommand.removeListener(onCommand);
+    };
+  }, []);
 
   return (
     <div className="profiles">
