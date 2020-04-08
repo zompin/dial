fda37d671ff9122ac66bf3107c9c531a51b0ea2a
Ilya Altukhov <zompin@ya.ru>
2020-04-09 00:05:58 +0300

    combobox key control

--- app/src/js/Components/ComboBox.tsx
+++ app/src/js/Components/ComboBox.tsx
@@ -24,7 +24,10 @@ const ComboBox = ({
   onHide,
 }: IProps) => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const [activeItem, setActiveItem] = React.useState<IBookmark | undefined>();
   const ref = React.useRef<HTMLDivElement>(null);
+  const itemsRef = React.useRef<IBookmark[]>([]);
+  const activeItemRef = React.useRef<IBookmark | undefined>();
 
   const onShowHandler = () => {
     if (onShow) {
@@ -42,10 +45,62 @@ const ComboBox = ({
     setIsOpen(false);
   };
 
-  const onEsc = ({ keyCode }: KeyboardEvent) => {
-    if (keyCode === 27) {
+  const onArrow = (key: string) => {
+    if (itemsRef.current.length === 0) {
+      return;
+    }
+
+    if (key !== 'ArrowDown' && key !== 'ArrowUp') {
+      return;
+    }
+
+    let index = itemsRef.current.findIndex(i => activeItemRef.current && i.id === activeItemRef.current.id);
+
+    if (key === 'ArrowDown') {
+      index += 1;
+
+      if (index === itemsRef.current.length) {
+        index = 0;
+      }
+    }
+
+    if (key === 'ArrowUp') {
+      index -= 1;
+
+      if (index < 0) {
+        index = itemsRef.current.length - 1;
+      }
+    }
+
+    setActiveItem(itemsRef.current[index]);
+
+    if (!ref.current) {
+      return;
+    }
+
+    const itemsElements = ref.current.querySelectorAll('.combobox__item');
+
+    if (!itemsElements || !itemsElements[index]) {
+      return;
+    }
+
+    if (index === 0 || index === itemsElements.length - 1) {
+      setTimeout(() => {
+        // @ts-ignore
+        itemsElements[index].focus();
+      }, 100);
+    } else {
+      // @ts-ignore
+      itemsElements[index].focus();
+    }
+  };
+
+  const onKey = ({ key }: KeyboardEvent) => {
+    if (key === 'Escape') {
       onHideHandler();
     }
+
+    onArrow(key);
   };
 
   const onOutsideClick = ({ target }: MouseEvent) => {
@@ -65,10 +120,21 @@ const ComboBox = ({
     onSelect(b);
   };
 
+  const onSelectHandlerByKey = ({ key, target }: React.KeyboardEvent, b: IBookmark) => {
+    if (key === 'Enter' || key === ' ') {
+      onSelectHandler(b);
+    }
+  };
+
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e);
     onShowHandler();
   };
+
+  React.useEffect(() => {
+    itemsRef.current = items;
+    activeItemRef.current = activeItem;
+  }, [items, activeItem]);
 
   React.useEffect(() => {
     if (items.length) {
@@ -80,11 +146,11 @@ const ComboBox = ({
 
   React.useEffect(() => {
     document.addEventListener('click', onOutsideClick);
-    document.addEventListener('keydown', onEsc);
+    document.addEventListener('keydown', onKey);
 
     return () => {
       document.removeEventListener('click', onOutsideClick);
-      document.removeEventListener('keydown', onEsc);
+      document.removeEventListener('keydown', onKey);
     };
   }, []);
 
@@ -96,14 +162,21 @@ const ComboBox = ({
         onChange={onChangeHandler}
         onFocus={onFocus}
       />
-      <div
-        className={cs('combobox__list', { combobox__list_open: isOpen })}
-      >
+      <div className={cs('combobox__list', { combobox__list_open: isOpen })}>
         {
           items.map((i) => (
-            <div className="combobox-item" key={i.id} onClick={() => onSelectHandler(i)}>
-              <div className="combobox-item__url">{i.url}</div>
-              <div className="combobox-item__title">{i.title}</div>
+            <div
+              key={i.id}
+              onClick={() => onSelectHandler(i)}
+              onKeyDown={(e) => onSelectHandlerByKey(e, i)}
+              onMouseEnter={() => setActiveItem(i)}
+              tabIndex={isOpen ? 0 : undefined}
+              className={cs('combobox__item', {
+                combobox__item_active: activeItem && activeItem.id === i.id,
+              })}
+            >
+              <div className="combobox__url">{i.url}</div>
+              <div className="combobox__title">{i.title}</div>
             </div>
           ))
         }
