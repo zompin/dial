78140bbfb9013dcf8cbb81e8f9aa9f7ffd57f784
Ilya Altukhov <zompin@ya.ru>
2020-04-17 18:09:45 +0300

    popup optimization

--- app/src/js/Components/Popup.tsx
+++ app/src/js/Components/Popup.tsx
@@ -10,15 +10,29 @@ interface IProps {
 }
 
 const Popup = ({ children, isOpen, onClose }: IProps) => {
+  const [isMounted, setIsMounted] = React.useState(isOpen);
+  const timerRef = React.useRef<ReturnType<typeof setTimeout>>();
+
   const onPopupClose = () => {
     onClose();
   };
 
-  const onEsc = (e: KeyboardEvent) => {
-    if (e.keyCode === 27) {
+  const onEsc = ({ key }: KeyboardEvent) => {
+    if (key === 'Escape') {
       onClose();
     }
   };
+
+  React.useEffect(() => {
+    const timer = timerRef.current;
+
+    if (isOpen) {
+      setIsMounted(true);
+      clearTimeout(timer);
+    } else {
+      timerRef.current = setTimeout(() => setIsMounted(false), 500);
+    }
+  }, [isOpen]);
 
   React.useEffect(() => {
     document.addEventListener('keydown', onEsc, true);
@@ -27,13 +41,17 @@ const Popup = ({ children, isOpen, onClose }: IProps) => {
     };
   }, []);
 
+  if (!isMounted) {
+    return null;
+  }
+
   return createPortal(
     <div className="popup">
       <div
-        className={cs('popup__cover', { popup__cover_show: isOpen })}
+        className={cs('popup__cover', { popup__cover_hide: !isOpen })}
         onClick={onPopupClose}
       />
-      <div className={cs('popup__inner', { popup__inner_show: isOpen })}>
+      <div className={cs('popup__inner', { popup__inner_hide: !isOpen })}>
         <XButton onClick={onPopupClose} action="close" className="popup" />
         {children}
       </div>
