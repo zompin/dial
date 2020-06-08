08ccf19c4b1a3158578df1d2cb393cf491bfe0bc
Ilya Altukhov <zompin@ya.ru>
2020-06-09 00:00:06 +0300

    logo

--- app/src/js/constants.ts
+++ app/src/js/constants.ts
@@ -3,6 +3,6 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const NAMES = {
   ROOT_FOLDER: 'unfiled_____',
-  APP_FOLDER: isDevelopment ? 'Dial_dev' : 'Dial',
+  APP_FOLDER: isDevelopment ? 'Rokot Dial Dev' : 'Rokot Dial',
   PROFILES_DEFAULT: 'Default',
 };
